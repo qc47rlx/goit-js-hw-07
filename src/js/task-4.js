0b3738cc15c7e5b408b87c241a28c2d752977f19
Anton Postnov <4qc7@proton.me>
2024-03-25 21:16:02 +0200

    finished

--- src/js/task-4.js
+++ src/js/task-4.js
@@ -1,1 +1,24 @@
+ const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const emailInput = loginForm.elements['email'];
+    const passwordInput = loginForm.elements['password'];
+
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (!email || !password) {
+      alert('All form fields must be filled in');
+      return;
+    }
+
+    const formData = {
+      email,
+      password
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+  });
